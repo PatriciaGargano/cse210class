e960a1a68292cfc7744cd259a5038dfe33891164
pgarganouma <111454838+pgarganouma@users.noreply.github.com>
2024-01-13 10:53:31 -0300

    Prep4 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+ 
+        List<int> numbers = new List<int>();
+
+        int number=-1;
+
+        while (number !=0)
+         
+        { 
+            Console.WriteLine("Enter a list of numbers, type 0 when finished ");
+
+            string userResponse = Console.ReadLine();
+            number = int.Parse(userResponse);
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
     }
+
 }
